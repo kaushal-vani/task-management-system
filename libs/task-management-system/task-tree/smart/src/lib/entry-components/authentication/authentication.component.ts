73c8544886446795372b3c14e37b62cad40ebadb
kaushal2469 <kaushal2469@gmail.com>
2024-07-02 22:23:21 +0530

    pass ui pages to smart entry component

--- libs/task-management-system/task-tree/smart/src/lib/entry-components/authentication/authentication.component.ts
+++ libs/task-management-system/task-tree/smart/src/lib/entry-components/authentication/authentication.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthenticationPageComponent } from '@task-tree-ui';
 
 @Component({
   selector: 'lib-authentication',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AuthenticationPageComponent],
   templateUrl: './authentication.component.html',
   styleUrl: './authentication.component.scss',
 })
