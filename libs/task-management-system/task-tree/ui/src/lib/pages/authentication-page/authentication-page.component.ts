bf338596f261f5f6a11a7c524799451395b51c49
kaushal2469 <kaushal2469@gmail.com>
2024-07-02 21:52:21 +0530

    create authentication page

---
+++ libs/task-management-system/task-tree/ui/src/lib/pages/authentication-page/authentication-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-authentication-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './authentication-page.component.html',
+  styleUrl: './authentication-page.component.scss',
+})
+export class AuthenticationPageComponent {}
