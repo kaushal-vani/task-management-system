a0bcf013fcff713c4a8d9ad8a122c41e817febb6
kaushal2469 <kaushal2469@gmail.com>
2024-07-02 22:22:58 +0530

    update nx ui library index ts

---
+++ libs/task-management-system/task-tree/ui/src/index.ts
@@ -0,0 +1,14 @@
+/* Features */
+export * from './lib/features/add-email/add-email.component';
+export * from './lib/features/add-phone-number/add-phone-number.component';
+export * from './lib/features/edit-email/edit-email.component';
+export * from './lib/features/edit-phone-number/edit-phone-number.component';
+export * from './lib/features/login/login.component';
+export * from './lib/features/one-time-password/one-time-password.component';
+export * from './lib/features/sign-up/sign-up.component';
+
+/* Pages */
+
+export * from './lib/pages/authentication-page/authentication-page.component';
+export * from './lib/pages/home-page/home-page.component';
+export * from './lib/pages/task-page/task-page.component';
