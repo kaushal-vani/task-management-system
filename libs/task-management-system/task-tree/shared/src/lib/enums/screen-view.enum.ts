ffe5c26cb4f1ca3fa847314a4746db39cfa69a23
kaushal2469 <kaushal2469@gmail.com>
2024-07-06 01:09:35 +0530

    add SCREEN_VIEW and BUTTON_NAME enum

---
+++ libs/task-management-system/task-tree/shared/src/lib/enums/screen-view.enum.ts
@@ -0,0 +1,16 @@
+export const enum SCREEN_VIEW {
+  ADD_EMAIL = 'add-email',
+  ADD_PHONE_NUMBER = 'add-phone-number',
+  EDIT_EMAIL = 'edit-email',
+  EDIT_PHONE_NUMBER = 'edit-phone-number',
+  LOGIN = 'login',
+  ONE_TIME_PASSWORD = 'one-time-password',
+  SIGN_UP = 'sign-up',
+}
+
+export const enum BUTTON_NAME {
+  SUBMIT = 'submit',
+  CANCEL = 'cancel',
+  CONTINUE = 'continue',
+  VERIFY = 'verify',
+}
