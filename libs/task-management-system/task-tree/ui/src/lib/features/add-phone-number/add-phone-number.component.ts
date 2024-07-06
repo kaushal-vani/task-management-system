d3b885c0248afb8b995ad3e1d900a12310e13d7f
kaushal2469 <kaushal2469@gmail.com>
2024-07-06 07:04:25 +0530

    add phone number screen view set up

--- libs/task-management-system/task-tree/ui/src/lib/features/add-phone-number/add-phone-number.component.ts
+++ libs/task-management-system/task-tree/ui/src/lib/features/add-phone-number/add-phone-number.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ScreenAction, SCREEN_VIEW, BUTTON_NAME } from '@task-tree-shared';
 
 @Component({
   selector: 'lib-add-phone-number',
@@ -8,4 +9,14 @@ import { CommonModule } from '@angular/common';
   templateUrl: './add-phone-number.component.html',
   styleUrl: './add-phone-number.component.scss',
 })
-export class AddPhoneNumberComponent {}
+export class AddPhoneNumberComponent {
+  @Output() navigate = new EventEmitter<ScreenAction>();
+
+  changeView() {
+    this.navigate.emit({
+      currentView: SCREEN_VIEW.ADD_PHONE_NUMBER,
+      buttonName: BUTTON_NAME.SUBMIT,
+      nextView: SCREEN_VIEW.ADD_EMAIL,
+    });
+  }
+}
