bf173206ade9087eccd978584f7eca0a3fbc4fa1
kaushal2469 <kaushal2469@gmail.com>
2024-07-06 12:02:52 +0530

    create Sign up form

--- libs/task-management-system/task-tree/ui/src/lib/features/sign-up/sign-up.component.ts
+++ libs/task-management-system/task-tree/ui/src/lib/features/sign-up/sign-up.component.ts
@@ -1,22 +1,78 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { BUTTON_NAME, SCREEN_VIEW, ScreenAction } from '@task-tree-shared';
+
+// PrimeNG Modules
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { SCREEN_VIEW, BUTTON_NAME, ScreenAction } from '@task-tree-shared';
 
 @Component({
   selector: 'lib-sign-up',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+    CardModule,
+  ],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss',
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent {
-  @Output() navigate = new EventEmitter<ScreenAction>();
+  @Output() navigateAction = new EventEmitter<ScreenAction>();
+  @Output() submitAction = new EventEmitter<ScreenAction>();
+  signUpForm: FormGroup;
+  loading = false;
+
+  constructor(private fb: FormBuilder) {
+    this.signUpForm = this.fb.group(
+      {
+        username: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+      },
+      { validator: this.passwordMatchValidator }
+    );
+  }
+
+  passwordMatchValidator(form: FormGroup) {
+    return form.get('password')?.value === form.get('confirmPassword')?.value
+      ? null
+      : { mismatch: true };
+  }
+
+  onSubmit() {
+    this.loading = true;
+
+    setTimeout(() => {
+      if (this.signUpForm.valid) {
+        this.submitAction.emit({
+          currentView: SCREEN_VIEW.SIGN_UP,
+          buttonName: BUTTON_NAME.SUBMIT,
+        });
+        console.log('Form Submitted', this.signUpForm.value);
+      }
+      this.loading = false;
+    }, 2000);
+  }
 
   changeView() {
-    this.navigate.emit({
+    this.navigateAction.emit({
       currentView: SCREEN_VIEW.SIGN_UP,
       buttonName: BUTTON_NAME.SUBMIT,
       nextView: SCREEN_VIEW.LOGIN,
+      previousView: SCREEN_VIEW.SIGN_UP,
     });
   }
 }
