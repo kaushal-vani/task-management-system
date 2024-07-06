8402e3bb74e6368802b898857124d6815d27e3a0
kaushal2469 <kaushal2469@gmail.com>
2024-07-06 20:17:45 +0530

    edit email flow

--- libs/task-management-system/task-tree/ui/src/lib/features/edit-email/edit-email.component.ts
+++ libs/task-management-system/task-tree/ui/src/lib/features/edit-email/edit-email.component.ts
@@ -1,22 +1,67 @@
-import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ScreenAction, SCREEN_VIEW, BUTTON_NAME } from '@task-tree-shared';
+import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { BUTTON_NAME, SCREEN_VIEW, ScreenAction } from '@task-tree-shared';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'lib-edit-email',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './edit-email.component.html',
-  styleUrl: './edit-email.component.scss',
+  styleUrls: ['./edit-email.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule,
+    CardModule,
+  ],
 })
 export class EditEmailComponent {
-  @Output() navigateAction = new EventEmitter<ScreenAction>();
+  @Output() navigate = new EventEmitter<ScreenAction>();
+  @Output() back = new EventEmitter<ScreenAction>();
 
-  changeView() {
-    this.navigateAction.emit({
+  emailForm: FormGroup;
+  successMessage!: string;
+
+  constructor(private fb: FormBuilder) {
+    this.emailForm = this.fb.group({
+      emailAddress: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  get email() {
+    return this.emailForm.get('emailAddress');
+  }
+
+  saveEmailAddress() {
+    if (this.emailForm.valid) {
+      this.successMessage = 'Email address saved successfully.';
+      setTimeout(() => {
+        this.navigate.emit({
+          currentView: SCREEN_VIEW.EDIT_EMAIL,
+          buttonName: BUTTON_NAME.SAVE,
+          nextView: SCREEN_VIEW.VERIFICATION_COMPLETED,
+        });
+      }, 2000);
+    }
+  }
+
+  cancelEdit() {
+    this.emailForm.reset();
+    this.back.emit({
       currentView: SCREEN_VIEW.EDIT_EMAIL,
-      buttonName: BUTTON_NAME.SUBMIT,
-      nextView: SCREEN_VIEW.ADD_EMAIL,
+      buttonName: BUTTON_NAME.CANCEL,
+      previousView: SCREEN_VIEW.CHANGE_DEVICE,
     });
   }
 }
