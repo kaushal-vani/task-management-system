d807f7e4d38e7ddeb5f9289ee9df983a5fa85a3f
kaushal2469 <kaushal2469@gmail.com>
2024-07-06 07:04:06 +0530

    add email screen view set up

--- libs/task-management-system/task-tree/ui/src/lib/features/add-email/add-email.component.ts
+++ libs/task-management-system/task-tree/ui/src/lib/features/add-email/add-email.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ScreenAction, SCREEN_VIEW, BUTTON_NAME } from '@task-tree-shared';
 
 @Component({
   selector: 'lib-add-email',
@@ -8,4 +9,14 @@ import { CommonModule } from '@angular/common';
   templateUrl: './add-email.component.html',
   styleUrl: './add-email.component.scss',
 })
-export class AddEmailComponent {}
+export class AddEmailComponent {
+  @Output() navigate = new EventEmitter<ScreenAction>();
+
+  changeView() {
+    this.navigate.emit({
+      currentView: SCREEN_VIEW.ADD_EMAIL,
+      buttonName: BUTTON_NAME.SUBMIT,
+      nextView: SCREEN_VIEW.VERIFICATION_COMPLETED,
+    });
+  }
+}
