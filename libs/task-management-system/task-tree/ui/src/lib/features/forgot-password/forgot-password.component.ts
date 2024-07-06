8ccf1ec501c6b7fb1c90c8b4306a99ad49eac18c
kaushal2469 <kaushal2469@gmail.com>
2024-07-06 16:45:41 +0530

    forgot password flow

--- libs/task-management-system/task-tree/ui/src/lib/features/forgot-password/forgot-password.component.ts
+++ libs/task-management-system/task-tree/ui/src/lib/features/forgot-password/forgot-password.component.ts
@@ -1,11 +1,67 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+// PrimeNG Modules
+import { SCREEN_VIEW, BUTTON_NAME, ScreenAction } from '@task-tree-shared';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'lib-forgot-password',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+    CardModule,
+  ],
   templateUrl: './forgot-password.component.html',
-  styleUrl: './forgot-password.component.scss',
+  styleUrls: ['./forgot-password.component.scss'],
 })
-export class ForgotPasswordComponent {}
+export class ForgotPasswordComponent implements OnInit {
+  @Output() back = new EventEmitter<ScreenAction>();
+  @Output() verify = new EventEmitter<ScreenAction>();
+  forgotPasswordForm!: FormGroup;
+  loading = false;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.forgotPasswordForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  onSubmit() {
+    this.loading = true;
+    setTimeout(() => {
+      if (this.forgotPasswordForm.valid) {
+        this.verify.emit({
+          currentView: SCREEN_VIEW.FORGOT_PASSWORD,
+          buttonName: BUTTON_NAME.SUBMIT,
+        });
+        // Mock logic for password recovery (send email, etc.)
+        console.log('Submitting forgot password form:', this.forgotPasswordForm.value);
+      }
+      this.loading = false;
+    }, 2000);
+  }
+
+  navigateToLogin() {
+    this.back.emit({
+      buttonName: BUTTON_NAME.GO_TO_LOGIN,
+      currentView: SCREEN_VIEW.FORGOT_PASSWORD,
+      previousView: SCREEN_VIEW.LOGIN,
+    });
+  }
+}
