89ba02353935234232415dc545694a3ab6420e90
kaushal2469 <kaushal2469@gmail.com>
2024-07-06 12:03:30 +0530

    create routes for pages

--- apps/task-management-system/task-tree/main/src/app/app.routes.ts
+++ apps/task-management-system/task-tree/main/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Route } from '@angular/router';
+import { AuthenticationPageComponent, HomePageComponent, TaskPageComponent } from '@task-tree-ui';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: 'authenticate', component: AuthenticationPageComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'task', component: TaskPageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' },
+];
