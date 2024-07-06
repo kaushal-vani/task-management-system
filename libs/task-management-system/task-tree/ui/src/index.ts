5ff4a5a3528933cb6bb2202c098a68decfa1a201
kaushal2469 <kaushal2469@gmail.com>
2024-07-06 15:20:53 +0530

    update ui library index ts

--- libs/task-management-system/task-tree/ui/src/index.ts
+++ libs/task-management-system/task-tree/ui/src/index.ts
@@ -3,6 +3,7 @@ export * from './lib/features/add-email/add-email.component';
 export * from './lib/features/add-phone-number/add-phone-number.component';
 export * from './lib/features/edit-email/edit-email.component';
 export * from './lib/features/edit-phone-number/edit-phone-number.component';
+export * from './lib/features/forgot-password/forgot-password.component';
 export * from './lib/features/login/login.component';
 export * from './lib/features/one-time-password/one-time-password.component';
 export * from './lib/features/sign-up/sign-up.component';
@@ -11,4 +12,5 @@ export * from './lib/features/sign-up/sign-up.component';
 
 export * from './lib/pages/authentication-page/authentication-page.component';
 export * from './lib/pages/home-page/home-page.component';
+export * from './lib/pages/profile-page/profile-page.component';
 export * from './lib/pages/task-page/task-page.component';
