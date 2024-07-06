bf6dcc7492053cf6b40701c13721804ebbcc7dd4
kaushal2469 <kaushal2469@gmail.com>
2024-07-06 07:04:50 +0530

    edit phone number screen view set up

--- libs/task-management-system/task-tree/ui/src/lib/features/edit-phone-number/edit-phone-number.component.ts
+++ libs/task-management-system/task-tree/ui/src/lib/features/edit-phone-number/edit-phone-number.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ScreenAction, SCREEN_VIEW, BUTTON_NAME } from '@task-tree-shared';
 
 @Component({
   selector: 'lib-edit-phone-number',
@@ -8,4 +9,14 @@ import { CommonModule } from '@angular/common';
   templateUrl: './edit-phone-number.component.html',
   styleUrl: './edit-phone-number.component.scss',
 })
-export class EditPhoneNumberComponent {}
+export class EditPhoneNumberComponent {
+  @Output() navigate = new EventEmitter<ScreenAction>();
+
+  changeView() {
+    this.navigate.emit({
+      currentView: SCREEN_VIEW.EDIT_PHONE_NUMBER,
+      buttonName: BUTTON_NAME.SUBMIT,
+      nextView: SCREEN_VIEW.EDIT_EMAIL,
+    });
+  }
+}
