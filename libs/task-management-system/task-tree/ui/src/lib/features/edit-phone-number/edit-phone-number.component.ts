bdffc0fa92ec1f46d99f67238ec1b524f3d2cf6e
kaushal2469 <kaushal2469@gmail.com>
2024-07-06 20:17:31 +0530

    edit phone number flow

--- libs/task-management-system/task-tree/ui/src/lib/features/edit-phone-number/edit-phone-number.component.ts
+++ libs/task-management-system/task-tree/ui/src/lib/features/edit-phone-number/edit-phone-number.component.ts
@@ -1,22 +1,70 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { ScreenAction, SCREEN_VIEW, BUTTON_NAME } from '@task-tree-shared';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { BUTTON_NAME, SCREEN_VIEW, ScreenAction } from '@task-tree-shared';
 
 @Component({
   selector: 'lib-edit-phone-number',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './edit-phone-number.component.html',
-  styleUrl: './edit-phone-number.component.scss',
+  styleUrls: ['./edit-phone-number.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule,
+    CardModule,
+  ],
 })
 export class EditPhoneNumberComponent {
-  @Output() navigateAction = new EventEmitter<ScreenAction>();
+  @Output() navigate = new EventEmitter<ScreenAction>();
+  @Output() back = new EventEmitter<ScreenAction>();
 
-  changeView() {
-    this.navigateAction.emit({
+  phoneForm: FormGroup;
+  successMessage = '';
+
+  constructor(private fb: FormBuilder) {
+    this.phoneForm = this.fb.group({
+      phoneNumber: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(15),
+          Validators.pattern('^[0-9]+$'),
+        ],
+      ],
+    });
+  }
+
+  get phone() {
+    return this.phoneForm.get('phoneNumber');
+  }
+
+  savePhoneNumber() {
+    if (this.phoneForm.valid) {
+      this.successMessage = 'Phone number updated successfully!';
+      setTimeout(() => {
+        this.navigate.emit({
+          currentView: SCREEN_VIEW.EDIT_PHONE_NUMBER,
+          buttonName: BUTTON_NAME.SAVE,
+          nextView: SCREEN_VIEW.VERIFICATION_COMPLETED,
+        });
+      }, 2000);
+    }
+  }
+
+  cancelEdit() {
+    this.phoneForm.reset();
+    this.back.emit({
       currentView: SCREEN_VIEW.EDIT_PHONE_NUMBER,
-      buttonName: BUTTON_NAME.SUBMIT,
-      nextView: SCREEN_VIEW.EDIT_EMAIL,
+      buttonName: BUTTON_NAME.CANCEL,
+      previousView: SCREEN_VIEW.CHANGE_DEVICE,
     });
   }
 }
