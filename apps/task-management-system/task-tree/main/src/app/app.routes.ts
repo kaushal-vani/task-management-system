99f4f8c2cb394e0bc24f4424c9cf06d57e5a913e
kaushal2469 <kaushal2469@gmail.com>
2024-07-06 21:57:21 +0530

    update routes

--- apps/task-management-system/task-tree/main/src/app/app.routes.ts
+++ apps/task-management-system/task-tree/main/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Route } from '@angular/router';
-import { AuthenticationPageComponent, HomePageComponent, TaskPageComponent } from '@task-tree-ui';
+import { AuthenticationPageComponent, HomePageComponent, PageNotFoundComponent, ProfilePageComponent, TaskPageComponent } from '@task-tree-ui';
 
 export const appRoutes: Route[] = [
   { path: 'authenticate', component: AuthenticationPageComponent },
   { path: 'home', component: HomePageComponent },
   { path: 'task', component: TaskPageComponent },
+  { path: 'profile', component: ProfilePageComponent },
+  { path: '404', component: PageNotFoundComponent },
   { path: '', redirectTo: '/authenticate', pathMatch: 'full' },
-  { path: '**', redirectTo: '/task' },
+  { path: '**', redirectTo: '/404' },
 ];
