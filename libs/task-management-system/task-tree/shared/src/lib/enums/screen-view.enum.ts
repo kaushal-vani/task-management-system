5938e1feb26c248a55ba006782b223e3d72d09c1
kaushal2469 <kaushal2469@gmail.com>
2024-07-06 14:48:14 +0530

    add new screenView and buttonName

--- libs/task-management-system/task-tree/shared/src/lib/enums/screen-view.enum.ts
+++ libs/task-management-system/task-tree/shared/src/lib/enums/screen-view.enum.ts
@@ -3,16 +3,26 @@ export const enum SCREEN_VIEW {
   ADD_PHONE_NUMBER = 'add-phone-number',
   EDIT_EMAIL = 'edit-email',
   EDIT_PHONE_NUMBER = 'edit-phone-number',
+  FORGOT_PASSWORD = 'forgot-password',
   LOGIN = 'login',
   ONE_TIME_PASSWORD = 'one-time-password',
+  SELECT_VERIFICATION_METHOD = 'select-verification_method',
   SIGN_UP = 'sign-up',
-  VERIFICATION_COMPLETED = 'verification-completed'
+  VERIFICATION_COMPLETED = 'verification-completed',
 }
 
 export const enum BUTTON_NAME {
-  SUBMIT = 'submit',
   CANCEL = 'cancel',
   CONTINUE = 'continue',
+  FORGOT_PASSWORD = 'forgot-password',
+  GO_TO_LOGIN = 'go-to-login',
+  GO_TO_SIGN_UP = 'go-to-sign-up',
+  LOGIN = 'login',
+  LOGOUT = 'logout',
+  NEXT = 'next',
+  OTP_SUBMIT = 'otp-submit',
+  PREVIOUS = 'previous',
+  SIGN_UP = 'sign-up',
+  SUBMIT = 'submit',
   VERIFY = 'verify',
-  OTP_SUBMIT = 'otp-submit'
 }
