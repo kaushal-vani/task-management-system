2a7ab0e7e9ce47637189689e088c7567a59bea2e
kaushal2469 <kaushal2469@gmail.com>
2024-07-07 20:16:58 +0530

    update stat route in app

--- apps/task-management-system/task-tree/main/src/app/app.routes.ts
+++ apps/task-management-system/task-tree/main/src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Route } from '@angular/router';
-import { AuthenticationPageComponent, HomePageComponent, PageNotFoundComponent, ProfilePageComponent, TaskPageComponent } from '@task-tree-ui';
+import { AuthenticationPageComponent, HomePageComponent, PageNotFoundComponent, ProfilePageComponent, StatPageComponent, TaskPageComponent } from '@task-tree-ui';
 
 export const appRoutes: Route[] = [
-  { path: 'authenticate', component: AuthenticationPageComponent },
-  { path: 'home', component: HomePageComponent },
-  { path: 'task', component: TaskPageComponent },
-  { path: 'profile', component: ProfilePageComponent },
-  { path: '404', component: PageNotFoundComponent },
   { path: '', redirectTo: '/authenticate', pathMatch: 'full' },
   { path: '**', redirectTo: '/404' },
+  { path: '404', component: PageNotFoundComponent },
+  { path: 'authenticate', component: AuthenticationPageComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'profile', component: ProfilePageComponent },
+  { path: 'stat', component: StatPageComponent },
+  { path: 'task', component: TaskPageComponent },
 ];
