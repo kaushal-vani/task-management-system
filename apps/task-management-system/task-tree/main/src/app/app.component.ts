7b6a95e3ac8fc11896e33da8cb0a068fa2e2d672
kaushal2469 <kaushal2469@gmail.com>
2024-07-07 20:08:09 +0530

    add footer to app main component

--- apps/task-management-system/task-tree/main/src/app/app.component.ts
+++ apps/task-management-system/task-tree/main/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthenticationComponent } from '@task-tree-smart';
 import { HeaderComponent } from '@task-tree-shared';
+import { FooterComponent } from '@task-tree-shared';
 
 @Component({
     standalone: true,
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [AuthenticationComponent, RouterModule, HeaderComponent]
+    imports: [AuthenticationComponent, RouterModule, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'task-management-system-task-tree-main';
